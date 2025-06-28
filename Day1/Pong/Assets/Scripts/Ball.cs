e06d9038a69216de83e41d6be219ed8fccab7e65
Christian Martínez de la Rosa <christt105@gmail.com>
2025-06-28 10:48:07 +0200

    Day1 (Pong Unity) (#1)
    * Initial project (pong Unity)
    
    * Move to parent directory
    
    * typo folder
    
    * Initial Base
    
    * Close Game
    
    * Create README.md
    
    * Removed unused assets

---
+++ Day1/Pong/Assets/Scripts/Ball.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Ball : MonoBehaviour
+{
+    public float baseSpeed = 5f;
+    public float maxSpeed = Mathf.Infinity;
+    private Rigidbody2D rb;
+    public float CurrentSpeed { get; set; }
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        // Clamp the velocity of the ball to the max speed
+        var direction = rb.velocity.normalized;
+        CurrentSpeed = Mathf.Min(CurrentSpeed, maxSpeed);
+        rb.velocity = direction * CurrentSpeed;
+    }
+
+    public void ResetPosition()
+    {
+        rb.velocity = Vector2.zero;
+        rb.position = Vector2.zero;
+    }
+
+    public void StartGame()
+    {
+        // Random horizontal
+        var x = Random.Range(0, 2) == 0 ? -1f : 1f;
+
+        // Random vertical
+        var y = Random.Range(0, 2) == 0
+            ? Random.Range(-1f, -0.5f)
+            : Random.Range(0.5f, 1f);
+
+        var direction = new Vector2(x, y).normalized;
+        rb.AddForce(direction * baseSpeed, ForceMode2D.Impulse);
+        CurrentSpeed = baseSpeed;
+    }
+}
---
+++ Day1/Pong/Assets/Scripts/ScoringZone.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class ScoringZone : MonoBehaviour
+{
+    public Paddle.Player player;
+    public GameManager gameManager;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ball")) {
+            gameManager.OnScored(player);
+        }
+    }
+
+}
