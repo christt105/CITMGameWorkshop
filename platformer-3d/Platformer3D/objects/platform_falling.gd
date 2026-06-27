b09797b71376bc80513e1e1ff0c5234b4ebf02f1
Christian Martínez de la Rosa <christt105@gmail.com>
2026-06-28 00:18:07 +0200

    Millorar comentaris dels TODOs conceptualment (sintaxi, mètodes i paràmetres) a Unity i Godot

--- platformer-3d/Platformer3D/objects/platform_falling.gd
+++ platformer-3d/Platformer3D/objects/platform_falling.gd
@@ -8,6 +8,10 @@ func _process(delta):
 	scale = scale.lerp(Vector3(1, 1, 1), delta * 10) # Animate scale
 	
 	# TODO 6: Fes que caigui la plataforma si s'ha activat 'falling' (aplica gravetat i resta la posició Y)
+	#   - Comprova si 'falling' és cert amb un condicional: if falling:
+	#   - Si és cert:
+	#     1. Incrementa la velocitat de caiguda aplicant gravetat: gravity += falling_speed * delta
+	#     2. Resta aquest efecte de la coordenada Y per desplaçar-la cap avall: position.y -= gravity * delta
 	# <SOL>
 	if falling:
 		gravity += falling_speed * delta
@@ -25,6 +29,7 @@ func _on_body_entered(_body):
 		scale = Vector3(1.25, 1, 1.25) # Animate scale
 	
 	# TODO 7: Quan el jugador toqui la plataforma, activa la variable 'falling' a true
+	#   - Assigna a la variable booleana 'falling' el valor de 'true'.
 	# <SOL>
 	falling = true
 	# </SOL>
--- platformer-3d/Platformer3D/scripts/hud.gd
+++ platformer-3d/Platformer3D/scripts/hud.gd
@@ -2,6 +2,8 @@ extends CanvasLayer
 
 func _on_coin_collected(coins):
 	# TODO 5: Actualitza el número de monedes a la interfície
+	#   - Assigna a la propietat '.text' del node de monedes ($Coins) el valor de monedes.
+	#   - Converteix el valor numèric a text amb la funció: str(coins)
 	# <SOL>
 	$Coins.text = str(coins)
 	# </SOL>
